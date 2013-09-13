a725d6d9396d587d7966414734dc40744e59aba9
Alistair Black <alistair.x.black@i.tescobank.com>
2013-09-13 14:51:51 +0100

    ali's files

---
+++ src/Station.java
@@ -0,0 +1,16 @@
+
+public class Station extends Property{
+
+	public Station(Group group, int purchasePrice, int mortgageValue,
+			int baseRent) {
+		super(group, purchasePrice, mortgageValue, baseRent);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getRentAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
