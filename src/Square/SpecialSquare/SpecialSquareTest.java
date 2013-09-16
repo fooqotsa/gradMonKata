ab78a0f062aa59e38ac6dc1de10f155ac73881e8
Leanne Gallacher <leanne.gallacher@i.tescobank.com>
2013-09-16 11:58:57 +0100

    Update to Special Square Test

--- src/Square/SpecialSquare/SpecialSquareTest.java
+++ src/Square/SpecialSquare/SpecialSquareTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 public class SpecialSquareTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOccupied() {
+		SpecialSquare square = new SpecialSquare();
+		square.landOn();
+		assertTrue(square.occupied);
 	}
 
 }
